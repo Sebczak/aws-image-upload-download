5cc7a0df94476151fb30fc4d812ea371de17732e
Sebastian Lachut <seb.lachut@gmail.com>
2024-04-06 22:30:25 +0200

    Add backend

--- src/main/java/com/company/awsimageupload/controllers/UserProfileController.java
+++ src/main/java/com/company/awsimageupload/controllers/UserProfileController.java
@@ -1,2 +1,37 @@
-package com.company.awsimageupload.controllers;public class UserProfileController {
+package com.company.awsimageupload.controllers;
+
+import com.company.awsimageupload.profile.UserProfile;
+import com.company.awsimageupload.services.UserProfileService;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("api/v1/user-profile")
+@CrossOrigin("*")
+public class UserProfileController {
+
+    private final UserProfileService userProfileService;
+
+    public UserProfileController(UserProfileService userProfileService) {
+        this.userProfileService = userProfileService;
+    }
+
+    @GetMapping
+    public List<UserProfile> getUserProfiles() {
+        return userProfileService.getUserProfiles();
+    }
+
+    @PostMapping(
+            path = "{userProfileId}/image/upload",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public void uploadUserProfileImage(@PathVariable("userProfileId") UUID userProfileId,
+                                       @RequestParam("file") MultipartFile file) {
+        userProfileService.uploadUserProfileImage(userProfileId, file);
+    }
 }
--- src/main/java/com/company/awsimageupload/services/UserProfileDataAccessService.java
+++ src/main/java/com/company/awsimageupload/services/UserProfileDataAccessService.java
@@ -1,2 +1,21 @@
-package com.company.awsimageupload.services;public class UserProfileDataAccessService {
+package com.company.awsimageupload.services;
+
+import com.company.awsimageupload.datastore.UserProfileDataStore;
+import com.company.awsimageupload.profile.UserProfile;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserProfileDataAccessService {
+
+    private final UserProfileDataStore userProfileDataStore;
+
+    public UserProfileDataAccessService(UserProfileDataStore userProfileDataStore) {
+        this.userProfileDataStore = userProfileDataStore;
+    }
+
+    List<UserProfile> getUserProfiles() {
+        return userProfileDataStore.getUserProfiles();
+    }
 }
